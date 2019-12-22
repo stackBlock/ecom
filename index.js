d718a3a7da6eb833c7febe1a413dc1f1d2a68a26
stackblock <lambdaTL.anthony@gmail.com>
2019-12-22 00:00:00 -0500

    parsed input code

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 const express = require("express");
+const log = console.log;
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("hi there");
+  res.send(`
+  <div>
+    <form method="POST">
+      <input name="email" placeholder="email" />
+      <input name="password" placeholder="password" />
+      <input name="passwordConfirmation" placeholder="password confirmation" />
+      <button>Sign Up</button>
+    </form>
+  </div>
+  `);
 });
 
-app.listen(3000, () => {
+app.post("/", (req, res) => {
+  req.on("data", data => {
+    const parsed = data.toString("utf8").split("&");
+    const formData = {};
+    for (let pair of parsed) {
+      const [key, value] = pair.split("=");
+      formData[key] = value;
+    }
+    log(formData);
+  });
+});
+
+app.listen(3001, () => {
   console.log("listening");
 });
